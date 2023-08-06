f7400f42f13a4a11413e18a6dc432622e4651083
Madhan Kumar <106460217+Madhanjetti@users.noreply.github.com>
2023-08-06 17:40:36 +0530

    Add files via upload

---
+++ MaxndMin Array.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+$numbers = array(5, 12, 9, 7, 3);
+
+$max = max($numbers);
+$min = min($numbers);
+
+echo "Maximum value: " . $max . "<br>";
+echo "Minimum value: " . $min . "<br>";
+
+
+
+?>
+
+</body>
+</html>

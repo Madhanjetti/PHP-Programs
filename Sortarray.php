267717fcf9755744b76f456e092c20af25437e85
Madhan Kumar <106460217+Madhanjetti@users.noreply.github.com>
2023-08-06 17:43:27 +0530

    Add files via upload

---
+++ Sortarray.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+$numbers = array(10, 4, 8, 2, 6);
+
+sort($numbers);
+
+echo "Sorted array in ascending order: ";
+print_r($numbers);
+
+
+
+
+
+?>
+
+</body>
+</html>

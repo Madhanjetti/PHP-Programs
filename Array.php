0cbb399be5dfe504721ddd9ff68cda13851fad92
Madhan Kumar <106460217+Madhanjetti@users.noreply.github.com>
2023-08-06 17:44:17 +0530

    Add files via upload

---
+++ Array.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+$fruits = array("Apple", "Banana", "Orange", "Mango");
+
+foreach ($fruits as $fruit) {
+    echo $fruit . "<br>";
+}
+
+
+
+
+
+
+?>
+
+</body>
+</html>
